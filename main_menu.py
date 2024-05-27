b78f20a35a2fcb0caedaafda7969d83dc91fe981
NekoNickNameNiko <131145275+NekoNickNameNiko@users.noreply.github.com>
2024-05-27 16:53:25 +0000

    update texte shadow

--- main_menu.py
+++ main_menu.py
@@ -3,11 +3,9 @@ import pygame
 import os
 
 start_btn = pygame.transform.scale(pygame.image.load(os.path.join("game_img/game_asset", "button_play.png")).convert_alpha(),(300,80))
-
-text_accueil = pygame.font.Font(None, 100).render("Lost World", True, (255, 255, 255))
+text_accueil = pygame.font.SysFont("fantasy", 55).render("Lost World", True, (255, 255, 255))
 text_rect_accueil = text_accueil.get_rect()
-text_rect_accueil.center = 1200/2, 425
-
+print(text_rect_accueil)
 
 class MainMenu:
     def __init__(self, fenetre):
@@ -16,7 +14,7 @@ class MainMenu:
         self.bg = pygame.image.load(os.path.join("game_img/game_asset", "start_menu_bg.jpg"))
         self.bg = pygame.transform.scale(self.bg, (self.width, self.height))
         self.fenetre = fenetre
-        self.btn = (self.width/3, 450, start_btn.get_width(), start_btn.get_height())
+        self.btn = (self.width/2- pygame.Surface.get_width(start_btn)/2, 450, start_btn.get_width(), start_btn.get_height())
 
     def run(self):
         run = True
@@ -42,6 +40,16 @@ class MainMenu:
 
     def draw(self):
         self.fenetre.blit(self.bg, (0,0))
-        self.fenetre.blit(text_accueil,text_rect_accueil)
+        self.dropShadowText("Lost World",100,410,300)
         self.fenetre.blit(start_btn, (self.btn[0], self.btn[1]))
         pygame.display.update()
+
+    def dropShadowText(self, text, size, x, y, colour=(255,255,255), drop_colour=(128,128,128), font=None):
+        dropshadow_offset = 1 + (size // 15)
+        text_font = pygame.font.Font(font, size)
+        
+        text_bitmap = text_font.render(text, True, drop_colour)
+        self.fenetre.blit(text_bitmap, (x+dropshadow_offset, y+dropshadow_offset) )
+        
+        text_bitmap = text_font.render(text, True, colour)
+        self.fenetre.blit(text_bitmap, (x, y) )
